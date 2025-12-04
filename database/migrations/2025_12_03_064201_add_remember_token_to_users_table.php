aa0b653e019b01c4ed05eb6bb6b758dd1e110db7
M1au-H <rapitokokokok@gmail.com>
2025-12-04 16:58:28 +0700

    autofeed

--- database/migrations/2025_12_03_064201_add_remember_token_to_users_table.php
+++ database/migrations/2025_12_03_064201_add_remember_token_to_users_table.php
@@ -4,20 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            if (!Schema::hasColumn('users', 'remember_token')) {
-                $table->rememberToken();
-            }
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken(); // WAJIB ADA UNTUK FILAMENT
+            $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('remember_token');
-        });
+        Schema::dropIfExists('users');
     }
 };
